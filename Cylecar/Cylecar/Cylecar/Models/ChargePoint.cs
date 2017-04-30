24756d59956f02fcc6865044892f76a10760cc53
alejandro de diego escribano <alex_el_viru@hotmail.com>
2017-04-30 13:58:04 +0200

    Agregar modelo punto recarga y servicio de rest(comentado)

---
+++ Cylecar/Cylecar/Cylecar/Models/ChargePoint.cs
@@ -0,0 +1,25 @@
+﻿using Java.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cylecar.Models
+{
+    public class ChargePoint
+    {
+        public int IdPunto { get; set; }
+        public int MennekesType { get; set; }
+        public int SchukoType { get; set; }
+        public string PointDesc { get; set; }
+        public bool PublicPoint { get; set; }
+        public string Place { get; set; }
+        public string Adress { get; set; }
+        public int PostalCode { get; set; }
+        public string City { get; set; }
+        //public Geopoint Location { get; set; }
+        public Date LastUpdate { get; set; }
+        public Uri Link { get; set; }
+    }
+}
---
+++ Cylecar/Cylecar/Cylecar/Services/RestService.cs
@@ -0,0 +1,43 @@
+﻿using Cylecar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cylecar.Services
+{
+    class RestService
+    {
+
+        public List<ChargePoint> Items { get; private set; }
+
+        /* public async Task<List<ChargePoint>> RefreshDataAsync()
+         {
+             /*HttpClient httpClient = new HttpClient();
+             httpClient.DefaultRequestHeaders.Accept.TryParseAdd("application/json");
+             //http://www.datosabiertos.jcyl.es/web/jcyl/risp/es/energia/vehiculo_electrico/1284273412751.json
+
+             Items = new List<ChargePoint>();
+             // uri = http://www.datosabiertos.jcyl.es/web/jcyl/risp/es/energia/vehiculo_electrico/1284273412751.json
+
+             var uri = new Uri("http://www.datosabiertos.jcyl.es/web/jcyl/risp/es/energia/vehiculo_electrico/1284273412751.json");
+
+             try
+             {
+                 var response = await httpClient.GetAsync(uri);
+                 if (response.IsSuccessStatusCode)
+                 {
+                     var content = await response.Content.ReadAsStringAsync();
+                     Items = JsonConvert.DeserializeObject<List<ChargePoint>>(content);
+                 }
+             }
+             catch (Exception ex)
+             {
+                 Debug.WriteLine(@"				ERROR {0}", ex.Message);
+             }
+
+             return Items;
+         }*/
+    }
+}
