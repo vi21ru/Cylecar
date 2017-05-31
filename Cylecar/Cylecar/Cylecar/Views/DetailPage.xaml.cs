63d4eb1ee395ec33472e8791ac4801b30c6b57d2
alejandro de diego escribano <alex_el_viru@hotmail.com>
2017-05-31 21:48:53 +0200

    cambios uwp

--- Cylecar/Cylecar/Cylecar/Views/DetailPage.xaml.cs
+++ Cylecar/Cylecar/Cylecar/Views/DetailPage.xaml.cs
@@ -25,32 +25,5 @@ namespace Cylecar.Views
         }
     }
 
-    class DetailPageViewModel : INotifyPropertyChanged
-    {
-
-        public DetailPageViewModel()
-        {
-            IncreaseCountCommand = new Command(IncreaseCount);
-        }
-
-        int count;
-
-        string countDisplay = "You clicked 0 times.";
-        public string CountDisplay
-        {
-            get { return countDisplay; }
-            set { countDisplay = value; OnPropertyChanged(); }
-        }
-
-        public ICommand IncreaseCountCommand { get; }
-
-        void IncreaseCount() =>
-            CountDisplay = $"You clicked {++count} times";
-
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-    }
+    
 }
